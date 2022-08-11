d6e70136e8acb93038951fc069f5e9c36f888e0d
MichalLinde <93143413+MichalLinde@users.noreply.github.com>
2022-08-11 15:44:55 +0200

    Initial Commit

---
+++ marvel-app-swiftui/marvel_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_app_swiftuiApp.swift
+//  marvel-app-swiftui
+//
+//  Created by Michal on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct marvel_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
